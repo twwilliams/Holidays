50e65792ee0aa8e0157ecb014327c06cb529f5c9
Tommy Williams <tommy@sapid.com>
2018-02-13 22:58:54 -0700

    Create basic structure

--- Holidays/Program.cs
+++ Holidays/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Holidays
 {
@@ -10,6 +12,19 @@ namespace Holidays
     {
         static void Main(string[] args)
         {
+            try
+            {
+                using (StreamReader sr = new StreamReader("2018-holidays.json"))
+                {
+                    string line = sr.ReadToEnd();
+                    Console.WriteLine(line);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("The holidays file couldn't be read");
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
